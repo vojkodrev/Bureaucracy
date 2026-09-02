97ff1d252aa53f83df00d9b60a1a2048ed6a6865
Vojko Drev <vojkodrev@gmail.com>
2026-09-02 11:00:52 +0200

    Remove existing frontend design

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import LayoutPage from './pages/LayoutPage.tsx'
-import InvoiceDetailsPage from './pages/InvoiceDetailsPage.tsx'
-import InvoicePage from './pages/InvoicePage.tsx'
 import InvoiceSearchPage from './pages/InvoiceSearchPage.tsx'
 import './index.css'
 
@@ -14,8 +12,6 @@ createRoot(document.getElementById('root')).render(
                 element={<Navigate to="/invoices/search" replace />}
             />
             <Route element={<LayoutPage />}>
-                <Route path="/invoice" element={<InvoicePage />} />
-                <Route path="/invoice/:id" element={<InvoiceDetailsPage />} />
                 <Route
                     path="/invoices/search"
                     element={<InvoiceSearchPage />}
