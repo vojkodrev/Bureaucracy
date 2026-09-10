4ef99a15ea2b10ec4c46aefc5701a60991680a0f
Vojko Drev <vojkodrev@gmail.com>
2026-09-10 15:03:19 +0200

    Allow production preview hostname

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,9 @@ const dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    preview: {
+        allowedHosts: ['drevi-pc'],
+    },
     resolve: {
         alias: {
             '@': path.resolve(dirname, './src'),
